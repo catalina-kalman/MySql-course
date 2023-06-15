ad17e32fd587bfcb04e734806dc2b188164c9916
Catalina Kalman <kalmanmariacatalina@gmail.com>
2023-06-15 09:23:15 +0100

    Relational databases exercise - query demo for employee database

--- Database Manipulation - basics/EmployeeDatabase_QueryDemo.sql
+++ Database Manipulation - basics/EmployeeDatabase_QueryDemo.sql
@@ -15,7 +15,7 @@ from employee;
 use employee_database;
 
 select
-employee_id "Emp #", last_name "Employee", job_id "Job", hire_date "Startdate"
+employee_id "Emp #", last_name "Employee", job_id "Job", hire_date "Hire Date"
 from employee;
 
 /*
@@ -32,5 +32,76 @@ from employee;
 Separate each column output by a comma. Name the column title THE_OUTPUT.
 */
 use employee_database;
-select concat(employee_id, ", ", first_name, ", ", last_name, ", ", email, ", ", phone_number, ", ", hire_date, ", ", job_id, ", ", salary, ", ", commission_pct, ", ", manager_id, ", ", department_id, ", ");
 
+select concat(employee_id, ", ", first_name, ", ", last_name, ", ", email, ", ", phone_number, ", ", hire_date, ", ",
+job_id, ", ", salary, ", ", commission_pct, ", ", manager_id, ", ", department_id, ", ") "THE_OUTPUT"
+from employee;
+
+/*
+1.	Due to budget issues, the HR department needs a report that displays
+the last name and salary of employees who earn more than $12,000.
+*/
+use employee_database;
+
+select last_name, salary
+from employee
+where salary>12000;
+
+/*
+2.	Create a report that displays the last name and department number
+for employee number 17.
+*/
+use employee_database;
+
+select last_name, department_id
+from employee
+where employee_id = 17;
+
+/*
+3.	Create a report to display the last name, job ID, and start date
+for the employees with the last names of Brown and Anderson
+*/
+select last_name, job_id, hire_date
+from employee
+where last_name = "Brown" or last_name = "Anderson";
+
+/*
+4.	Display the last name and department number of all employees in
+departments 2 or 5.
+*/
+select last_name, department_id
+from employee
+where department_id = 2 or department_id = 5;
+
+/*
+5.	Display the last name and salary of employees who earn between $5,000
+and $45,000 and are in department 2 or 5. Label the columns Employee
+and Monthly Salary, respectively.
+*/
+select last_name "Employee", salary "Monthly Salary"
+from employee
+where salary between 5000 and 45000 and (department_id = 2 or department_id = 5);
+
+/*
+6.	The HR department needs a report that displays the last name
+and hire date for all employees who were hired in 2002.
+*/
+select last_name, hire_date
+from employee
+where hire_date between "2002-01-01" and "2002-12-31";
+
+/*
+7.	Create a report to display the last name and job title of all
+employees who do not have a manager.
+*/
+select last_name, job_id
+from employee
+where manager_id is null;
+
+/*
+8.	Create a report to display the last name, salary, and commission
+of all employees who earn commissions.
+*/
+select last_name, salary, commission_pct
+from employee
+where commission_pct is not null;
